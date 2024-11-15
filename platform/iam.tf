dda4107cbbee71c2f7a5fd431b57a3add77feebe
wakabaseisei <wakabaseisei@gmail.com>
2024-11-15 21:28:12 +0900

    feat: add aws role for terraform apply

--- platform/iam.tf
+++ platform/iam.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "github_actions" {
-  name = "github_actions"
+resource "aws_iam_role" "github_actions_plan" {
+  name = "github_actions_plan"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -23,7 +23,37 @@ resource "aws_iam_role" "github_actions" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "github_actions" {
-  role       = aws_iam_role.github_actions.name
+resource "aws_iam_role_policy_attachment" "github_actions_plan" {
+  role       = aws_iam_role.github_actions_plan.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_role" "github_actions_apply" {
+  name = "github_actions_apply"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect": "Allow",
+        "Principal": {
+            "Federated": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Action": "sts:AssumeRoleWithWebIdentity",
+        "Condition": {
+            "StringLike": {
+                "token.actions.githubusercontent.com:sub": "repo:wakabaseisei/ms-infra:ref:refs/heads/main"
+            },
+            "StringEquals": {
+                "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+            }
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_apply" {
+  role       = aws_iam_role.github_actions_apply.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
