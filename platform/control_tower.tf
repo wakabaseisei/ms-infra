0092a803c620ca13c4944ed808cbaf289246db88
wakabaseisei <wakabaseisei@gmail.com>
2024-11-19 00:04:01 +0900

    feat: add iam role for control tower

---
+++ platform/control_tower.tf
@@ -0,0 +1,134 @@
+resource "aws_iam_role" "controltower_admin" {
+  name = "AWSControlTowerAdmin"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "controltower.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  path = "/service-role/"
+}
+
+resource "aws_iam_role_policy_attachment" "controltower_admin_managed" {
+  role       = aws_iam_role.controltower_admin.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSControlTowerServiceRolePolicy"
+}
+
+resource "aws_iam_policy" "controltower_admin" {
+  name = "AWSControlTowerAdminPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "ec2:DescribeAvailabilityZones"
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "controltower_admin_custom" {
+  role       = aws_iam_role.controltower_admin.name
+  policy_arn = aws_iam_policy.controltower_admin.arn
+}
+
+resource "aws_iam_role" "controltower_cloudtrail" {
+  name = "AWSControlTowerCloudTrailRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "cloudtrail.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  path = "/service-role/"
+}
+
+resource "aws_iam_policy" "controltower_cloudtrail" {
+  name = "AWSControlTowerCloudTrailRolePolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      Resource = "arn:aws:logs:*:*:log-group:aws-controltower/CloudTrailLogs:*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "controltower_cloudtrail" {
+  role       = aws_iam_role.controltower_cloudtrail.name
+  policy_arn = aws_iam_policy.controltower_cloudtrail.arn
+}
+
+resource "aws_iam_role" "controltower_config" {
+  name = "AWSControlTowerConfigAggregatorRoleForOrganizations"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "config.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  path = "/service-role/"
+}
+
+resource "aws_iam_role_policy_attachment" "controltower_config" {
+  role       = aws_iam_role.controltower_config.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRoleForOrganizations"
+}
+
+resource "aws_iam_role" "controltower_stackset" {
+  name = "AWSControlTowerStackSetRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "cloudformation.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+  path = "/service-role/"
+}
+
+resource "aws_iam_policy" "controltower_stackset" {
+  name = "AWSControlTowerStackSetRolePolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "sts:AssumeRole"
+      Resource = "arn:aws:iam::*:role/AWSControlTowerExecution"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "controltower_stackset" {
+  role       = aws_iam_role.controltower_stackset.name
+  policy_arn = aws_iam_policy.controltower_stackset.arn
+}
