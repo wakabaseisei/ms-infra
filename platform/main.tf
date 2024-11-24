920b191c1163fb2a614a6bca512ea127297a7701
wakabaseisei <wakabaseisei@gmail.com>
2024-11-24 11:34:27 +0900

    refactor: Transition to module-based Terraform structure

---
+++ platform/main.tf
@@ -0,0 +1,8 @@
+data "aws_organizations_organization" "main" {}
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+module "github" {
+    source = "../modules/github"
+    account_id = data.aws_caller_identity.current
+}
--- services/api-front/dev/openid_connect_provider.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_openid_connect_provider" "github_actions_deploy" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["thumbprintthumbprintthumbprintthumbprint"]
-}
