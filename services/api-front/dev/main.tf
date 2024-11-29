8e6ea4b4f17e0e61a67945be64f6f1ca14a83cc2
wakabaseisei <wakabaseisei@gmail.com>
2024-11-29 21:41:47 +0900

    remove: delete api-front OIDC Provider resource

--- services/api-front/dev/main.tf
+++
@@ -1,7 +0,0 @@
-data "aws_caller_identity" "current" {}
-data "aws_region" "current" {}
-
-module "github" {
-    source = "../../../modules/github"
-    account_id = data.aws_caller_identity.current.account_id
-}
