8e55e8b365e08c54fe81a46499323a9888feda21
wakabaseisei <wakabaseisei@gmail.com>
2024-12-16 20:58:46 +0900

    chore: change Fargate mode to Auto mode

--- services/_common/dev/main.tf
+++ services/_common/dev/main.tf
@@ -32,12 +32,24 @@ module "vpc" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+
   version = "~> 20.31"
   cluster_version = "1.31"
+
   cluster_name    = "${local.env}-eks"
+
   vpc_id          = module.vpc.vpc_id
-  enable_irsa     = true
   subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
 }
 
 module "cluster" {
