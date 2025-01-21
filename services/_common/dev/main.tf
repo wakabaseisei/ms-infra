e0f525c9205c8407ea825728ba507d47f93619d1
wakabaseisei <wakabaseisei@gmail.com>
2025-01-21 22:17:34 +0900

    add cluster access entry

--- services/_common/dev/main.tf
+++ services/_common/dev/main.tf
@@ -71,6 +71,19 @@ module "eks" {
         }
       }
     }
+
+    plan = {
+      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github_actions_plan"
+
+      policy_associations = {
+        clusteradmin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type       = "cluster"
+          }
+        }
+      }
+    }
   }
 }
 
@@ -135,9 +148,9 @@ resource "aws_security_group" "vpc_endpoints" {
   }
 }
 
-module "cluster" {
-  source = "../../../modules/cluster"
-  depends_on = [
-    module.eks
-  ]
-}
+# module "cluster" {
+#   source = "../../../modules/cluster"
+#   depends_on = [
+#     module.eks
+#   ]
+# }
