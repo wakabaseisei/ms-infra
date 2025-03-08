40e072c0ccfd4a36adc65db2969306029d3ba0b4
wakabaseisei <wakabaseisei@gmail.com>
2025-03-08 23:30:05 +0900

    debug

--- services/api-front/dev/data.tf
+++ services/api-front/dev/data.tf
@@ -7,3 +7,8 @@ data "terraform_remote_state" "common" {
     region = "ap-northeast-1"
   }
 }
+
+
+data "aws_secretsmanager_secret_version" "rds_master_password" {
+  secret_id = module.aurora.rds_master_password_secret_arn
+}
