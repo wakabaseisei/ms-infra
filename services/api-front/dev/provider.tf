d2fad6f005f6a34574f6f2e37c117d08ff887d2a
wakabaseisei <wakabaseisei@gmail.com>
2025-03-08 23:18:53 +0900

    remove mysql provider

--- services/api-front/dev/provider.tf
+++ services/api-front/dev/provider.tf
@@ -4,19 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.81"
     }
-    mysql = {
-      source = "petoju/mysql"
-      version = "~> 3.0.70"
-    }
   }
 }
 
 provider "aws" {
   region = "ap-northeast-1"
-}
-
-provider "mysql" {
-  endpoint = "${module.aurora.db_cluster_endpoint}:3306"
-  username = module.aurora.rds_master_username
-  password = local.rds_master_password
 }
