61bc434b495d88793aa6f287ecfc8fb6339b445f
wakabaseisei <wakabaseisei@gmail.com>
2025-03-20 12:01:16 +0900

    chore: Enable Aurora

--- services/api-front/dev/main.tf
+++ services/api-front/dev/main.tf
@@ -15,20 +15,20 @@ module "tenant" {
 }
 
 // NOTE: Disabled to reduce costs. Uncomment out only when used.
-# module "aurora" {
-#   source = "../../../modules/db"
-#   cluster_identifier = local.service_name
-#   cluster_vpc_id = data.terraform_remote_state.common.outputs.vpc_id
-#   cluster_instances_subnet_ids = data.terraform_remote_state.common.outputs.database_subnets
-#   database_name = local.service_name_letter
-#   reader_instance_classes = [ "db.serverless", "db.serverless" ]
-#   database_username = local.service_name
-#   account_id = data.aws_caller_identity.current.account_id
-#   migration_lambda = {
-#     # CIで置き換えるため、仮のURLとして設定
-#     image_url = "dummy"
-#     # CIで置き換えるため、仮のタグとして設定
-#     image_tag = "dummy"
-#     entry_point = ["/bin/migrate-lambda"]
-#   }
-# }
+module "aurora" {
+  source = "../../../modules/db"
+  cluster_identifier = local.service_name
+  cluster_vpc_id = data.terraform_remote_state.common.outputs.vpc_id
+  cluster_instances_subnet_ids = data.terraform_remote_state.common.outputs.database_subnets
+  database_name = local.service_name_letter
+  reader_instance_classes = [ "db.serverless", "db.serverless" ]
+  database_username = local.service_name
+  account_id = data.aws_caller_identity.current.account_id
+  migration_lambda = {
+    # CIで置き換えるため、仮のURLとして設定
+    image_url = "dummy"
+    # CIで置き換えるため、仮のタグとして設定
+    image_tag = "dummy"
+    entry_point = ["/bin/migrate-lambda"]
+  }
+}
