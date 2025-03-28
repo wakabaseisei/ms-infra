bedefbdeb1d62012cf9eba4e64719994910789f6
wakabaseisei <wakabaseisei@gmail.com>
2025-03-28 21:27:46 +0900

    fix

--- services/api-front/dev/main.tf
+++ services/api-front/dev/main.tf
@@ -8,10 +8,10 @@ module "tenant" {
     account_id = data.aws_caller_identity.current.account_id
     github_repository_name = local.service_name
     // NOTE: Disabled to reduce costs. Uncomment out only when used.
-    eks = {
-      service_account_name = local.service_name
-      oidc_provider = data.terraform_remote_state.common.outputs.eks_oidc_provider
-    }
+    # eks = {
+    #   service_account_name = local.service_name
+    #   oidc_provider = data.terraform_remote_state.common.outputs.eks_oidc_provider
+    # }
 }
 
 // NOTE: Disabled to reduce costs. Uncomment out only when used.
@@ -31,8 +31,8 @@ module "aurora" {
     image_tag = "dev-20250327-235159-ff884ba"
     entry_point = ["/bin/migrate-lambda"]
   }
-  database_access_client = {
-    role = module.tenant.irsa_role_name
-    security_group_id = data.terraform_remote_state.common.outputs.eks_node_security_group_id
-  }
+#   database_access_client = {
+#     role = module.tenant.irsa_role_name
+#     security_group_id = data.terraform_remote_state.common.outputs.eks_node_security_group_id
+#   }
 }
