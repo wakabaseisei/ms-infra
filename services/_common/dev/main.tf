0c6d9291f7c0f107a8170aebb313d41efdd99871
wakabaseisei <wakabaseisei@gmail.com>
2025-04-12 18:00:32 +0900

    chore: Disable EKS Cluster and Helm

--- services/_common/dev/main.tf
+++ services/_common/dev/main.tf
@@ -24,7 +24,7 @@ module "vpc" {
   database_subnets = ["10.0.201.0/24", "10.0.202.0/24"]
 
   // NOTE: Disable to reduce cost when eks cluster is not used.
-  enable_nat_gateway = true
+  # enable_nat_gateway = true
 
   # For use with AWS Load Balancer Controller
   public_subnet_tags = {
@@ -37,125 +37,125 @@ module "vpc" {
 }
 
 # NOTE: For cost-saving purposes, the unused resources are commented out when they are not in use.
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+# module "eks" {
+#   source          = "terraform-aws-modules/eks/aws"
 
-  version = "~> 20.31"
-  cluster_version = "1.31"
+#   version = "~> 20.31"
+#   cluster_version = "1.31"
 
-  cluster_name    = "${local.env}-eks"
+#   cluster_name    = "${local.env}-eks"
 
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+#   vpc_id          = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true
-  cluster_endpoint_private_access = true
-  authentication_mode = "API"
+#   cluster_endpoint_public_access = true
+#   cluster_endpoint_private_access = true
+#   authentication_mode = "API"
 
-  enable_cluster_creator_admin_permissions = true
+#   enable_cluster_creator_admin_permissions = true
 
-  # EKS Auto Mode will create and delete EC2 Managed Instances
-  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster#compute_config
-  cluster_compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
-  }
+#   # EKS Auto Mode will create and delete EC2 Managed Instances
+#   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster#compute_config
+#   cluster_compute_config = {
+#     enabled    = true
+#     node_pools = ["general-purpose"]
+#   }
 
-  cloudwatch_log_group_retention_in_days = 3
+#   cloudwatch_log_group_retention_in_days = 3
 
-  access_entries = {
-    local = {
-      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/ap-northeast-1/AWSReservedSSO_AdministratorAccess_81e4d4a3d70e0c36"
+#   access_entries = {
+#     local = {
+#       principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-reserved/sso.amazonaws.com/ap-northeast-1/AWSReservedSSO_AdministratorAccess_81e4d4a3d70e0c36"
 
-      policy_associations = {
-        clusteradmin = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type       = "cluster"
-          }
-        }
-      }
-    }
+#       policy_associations = {
+#         clusteradmin = {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#           access_scope = {
+#             type       = "cluster"
+#           }
+#         }
+#       }
+#     }
 
-    plan = {
-      principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github_actions_plan"
+#     plan = {
+#       principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github_actions_plan"
 
-      policy_associations = {
-        clusteradmin = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type       = "cluster"
-          }
-        }
-      }
-    }
-  }
-}
+#       policy_associations = {
+#         clusteradmin = {
+#           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+#           access_scope = {
+#             type       = "cluster"
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
 
-resource "aws_vpc_endpoint" "ecr_api" {
-  vpc_id            = module.vpc.vpc_id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = module.vpc.private_subnets
-  security_group_ids = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
+# resource "aws_vpc_endpoint" "ecr_api" {
+#   vpc_id            = module.vpc.vpc_id
+#   service_name      = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids        = module.vpc.private_subnets
+#   security_group_ids = [aws_security_group.vpc_endpoints.id]
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "ECR-API-Endpoint"
-  }
-}
+#   tags = {
+#     Name = "ECR-API-Endpoint"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "ecr_dkr" {
-  vpc_id            = module.vpc.vpc_id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
-  vpc_endpoint_type = "Interface"
-  subnet_ids        = module.vpc.private_subnets
-  security_group_ids = [aws_security_group.vpc_endpoints.id]
-  private_dns_enabled = true
+# resource "aws_vpc_endpoint" "ecr_dkr" {
+#   vpc_id            = module.vpc.vpc_id
+#   service_name      = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
+#   vpc_endpoint_type = "Interface"
+#   subnet_ids        = module.vpc.private_subnets
+#   security_group_ids = [aws_security_group.vpc_endpoints.id]
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "ECR-DKR-Endpoint"
-  }
-}
+#   tags = {
+#     Name = "ECR-DKR-Endpoint"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id            = module.vpc.vpc_id
-  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
-  vpc_endpoint_type = "Gateway"
-  route_table_ids   = module.vpc.private_route_table_ids
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id            = module.vpc.vpc_id
+#   service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+#   vpc_endpoint_type = "Gateway"
+#   route_table_ids   = module.vpc.private_route_table_ids
 
-  tags = {
-    Name = "S3-Gateway-Endpoint"
-  }
-}
+#   tags = {
+#     Name = "S3-Gateway-Endpoint"
+#   }
+# }
 
-resource "aws_security_group" "vpc_endpoints" {
-  name        = "${local.env}-vpc-endpoints-sg"
-  description = "Security group for VPC endpoints"
-  vpc_id      = module.vpc.vpc_id
+# resource "aws_security_group" "vpc_endpoints" {
+#   name        = "${local.env}-vpc-endpoints-sg"
+#   description = "Security group for VPC endpoints"
+#   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = module.vpc.private_subnets_cidr_blocks
-  }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = module.vpc.private_subnets_cidr_blocks
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "${local.env}-vpc-endpoints-sg"
-  }
-}
+#   tags = {
+#     Name = "${local.env}-vpc-endpoints-sg"
+#   }
+# }
 
-module "cicd" {
-  source = "../../../modules/cicd"
-  depends_on = [
-    module.eks
-  ]
-}
+# module "cicd" {
+#   source = "../../../modules/cicd"
+#   depends_on = [
+#     module.eks
+#   ]
+# }
