13b5c70b026aaef404ab224c450cc61c40ed2755
wakabaseisei <wakabaseisei@gmail.com>
2025-05-04 14:32:33 +0900

    dix

--- modules/container/outputs.tf
+++ modules/container/outputs.tf
@@ -1,14 +1,14 @@
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with the cluster"
-  value       = try(aws_eks_cluster.this[0].certificate_authority[0].data, null)
+  value       = aws_eks_cluster.this.certificate_authority[0].data
 }
 
 output "cluster_endpoint" {
   description = "Endpoint for your Kubernetes API server"
-  value       = try(aws_eks_cluster.this[0].endpoint, null)
+  value       = aws_eks_cluster.this.endpoint
 }
 
 output "cluster_name" {
   description = "The name of the EKS cluster"
-  value       = try(aws_eks_cluster.this[0].name, "")
+  value       = aws_eks_cluster.this.name
 }
