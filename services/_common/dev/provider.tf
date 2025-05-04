4eec33122aed7ecf02e1d8d5f667545743b806fe
wakabaseisei <wakabaseisei@gmail.com>
2025-05-04 17:36:38 +0900

    i

--- services/_common/dev/provider.tf
+++ services/_common/dev/provider.tf
@@ -15,16 +15,16 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.container.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.container.cluster_certificate_authority_data)
+provider "helm" {
+  kubernetes {
+    host                   = module.container.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.container.cluster_certificate_authority_data)
 
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1beta1"
-#       command     = "aws"
-#       # This requires the awscli to be installed locally where Terraform is executed
-#       args = ["eks", "get-token", "--cluster-name", module.container.cluster_name]
-#     }
-#   }
-# }
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", module.container.cluster_name]
+    }
+  }
+}
